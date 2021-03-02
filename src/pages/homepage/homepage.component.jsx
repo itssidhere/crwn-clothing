6b7e6accb1f9db9f159430c02a52dc3da8393a17
itssidhere <sidjha0001@gmail.com>
2021-03-02 23:49:50 +0530

    broken homepage into components, finished styling menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+
+const HomePage = () => (
+  <div className='homepage'>
+   <Directory/>
+  </div>
+);
+
+export default HomePage;
