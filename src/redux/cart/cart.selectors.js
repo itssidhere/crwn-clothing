3d62d1a8eed808ce770615a6f59a12904442bd69
itssidhere <sidjha0001@gmail.com>
2021-03-06 13:58:27 +0530

    udpating our app with selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+const selectUser = (state) => state.user;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+
+export const selectCartItemsCount  = createSelector(
+    [selectCartItems],
+    cartItems =>  cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  )
+)
