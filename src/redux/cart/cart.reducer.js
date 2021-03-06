a1f3a6fa30c6488248195e80d3709451d293b5f9
itssidhere <sidjha0001@gmail.com>
2021-03-06 18:20:19 +0530

    add/ remove button added

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,28 +1,43 @@
-import CartActionTypes from './cart.types'
-import {addItemToCart}  from "./cart.utils";
+import { act } from "react-dom/test-utils";
+import { removeItem } from "./cart.actions";
+import CartActionTypes from "./cart.types";
+import { addItemToCart,  removeItemFromCart } from "./cart.utils";
 const INITIAL_STATE = {
-    hidden: true,
-    cartItems: []
-}
+  hidden: true,
+  cartItems: [],
+};
 
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
 
-const cartReducer = (state = INITIAL_STATE , action) =>{
-    switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
 
-        case CartActionTypes.ADD_ITEM:
-            return{
-                ...state,
-                cartItems: addItemToCart(state.cartItems, action.payload)
-            }
-    
-        default:
-            return state;
-    }
-}
+    case CartActionTypes.REMOVE_ITEM:
+        return {
+            ...state,
+            cartItems:removeItemFromCart(state.cartItems, action.payload)
+        }
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default cartReducer;
